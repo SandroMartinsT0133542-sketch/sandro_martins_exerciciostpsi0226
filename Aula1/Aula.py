a92ff2b37a21de4816164461af1c9f39d874f2b7
SandroMartinsT0133542-sketch <Sandro.Martins.T0133542@edu.atec.pt>
2026-03-03 17:10:22 +0000

    Aula 1 exec

--- Aula1/Aula.py
+++ Aula1/Aula.py
@@ -1,1 +1,25 @@
-print("Hello cenas")
+# exercicio encontra o maior e o menor
+import random
+
+val1 = random.randint(1, 100)
+val2 = random.randint(1, 100)
+val3 = random.randint(1, 100)
+print("Valor 1:", val1)
+print("Valor 2:", val2)
+print("Valor 3:", val3)
+# encontra o maior
+if val1 >= val2 and val1 >= val3:
+    maior = val1
+elif val2 >= val1 and val2 >= val3:
+    maior = val2
+else:
+    maior = val3
+# encontra o menor
+if val1 <= val2 and val1 <= val3:
+    menor = val1
+elif val2 <= val1 and val2 <= val3:
+    menor = val2
+else:
+    menor = val3
+print("O maior valor é:", maior)
+print("O menor valor é:", menor)
