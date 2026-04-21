9ba5868a653d22a82af8c4b1af07af7dbec37c06
SandroMartinsT0133542-sketch <Sandro.Martins.T0133542@edu.atec.pt>
2026-04-21 18:37:58 +0100

    add: update file handling to use JSON format

--- Exercicios/misc/exec_file.py
+++ Exercicios/misc/exec_file.py
@@ -1,37 +1,42 @@
 import os
+import json
+
+type Product = dict["name": str, "price": int]
+type Data = list[Product]
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CODE = 'utf-8'
 
+
 def initialize_program():
-  filename = os.path.join(BASE_DIR, 'data.txt')
-  text = ''
+  filename = os.path.join(BASE_DIR, 'data.json')
+  temp_data = []
 
   print("Welcome to the file reader and writer program!")
   print("This program will read the contents of a file and allow you to write new content to it.")
   print("Please make sure the file exists before running the program.")
   print("Let's get started!")
-  print("------------ Menu ------------")
-  print("1 - Write text to file")
-  print("2 - Read file")
-  print("3 - Write to file")
-  print("4 - Save file")
-  print("5 - List files in directory")
-  print("6 - Exit")
-  print("------------------------------")
 
   try:
     while True:
+      print("------------ Menu ------------")
+      print("1 - Write text to file")
+      print("2 - Read file")
+      print("3 - Write to file")
+      print("4 - Save file")
+      print("5 - List files in directory")
+      print("6 - Exit")
+      print("------------------------------")
       opt = input("Choose an option: ")
       match opt:
         case "1":
-          text = input("Enter the text you want to write to the file: ")
+          insert_data(temp_data);
         case "2":
           read_file(filename)
         case "3":
-          write_file(filename, text)
+          write_file(filename, temp_data)
         case "4":
-          create_file(filename, text)
+          create_file(filename, temp_data)
         case "5":
           print(f"Listing files in: {BASE_DIR}")
           list_files_in_directory(BASE_DIR)
@@ -43,44 +48,44 @@ def initialize_program():
   except Exception as e:
     print("An error occurred:", e)
 
+def insert_data(temp_data: list[Product]):
+  name = input("Enter product name: ")
+  price = int(input("Enter product price: "))
+  temp_data.append({"name": name, "price": price})
+
 def read_file(filename):
   if not os.path.exists(filename):
     print("File does not exist.")
   else:
     with open(filename, 'r', encoding=CODE) as file:
-      data = file.read()
-      print("File contents:")
-      print(data)
-      file.close()
-  
+      y = json.load(file)
+      print("File content:")
+      for product in y:
+        print(f"Name: {product['name']}, Price: {product['price']}")
 
-def write_file(filename, text):
-  with open(filename, 'w', encoding=CODE) as file:
-    file.write(text)
-    file.close()
-  print("Text written to file successfully.")
-
-
-
-def create_file(filename, text):
-  if os.path.exists(filename):
-    print("File already exists. Please choose a different name or delete the existing file.")
+def write_file(filename, data: list[Product]):
+  if not os.path.exists(filename):
+    print("File does not exist. Please create the file first.")
     return
   
-  with open(filename, 'x', encoding=CODE) as file:
-    file.write(text)
+  with open(filename, 'w', encoding=CODE) as file:
+    json.dump(data, file, indent=4)
     file.close()
-  print("File created and text written successfully.")
 
-def list_files_in_directory(directory):
-  files = sorted(os.listdir(directory))
+def create_file(filename, data: list[Product]):
+  with open(filename, 'w', encoding=CODE) as file:
+    json.dump(data, file, indent=4)
+    file.close()
+    print(f"File '{filename}' created successfully.")
+
+def list_files_in_directory(directory: str):
+  files = os.listdir(directory)
   if not files:
     print("No files found in the directory.")
-    return
-  
-  print("Files in directory:")
-  for file in files:
-    print(file)
+  else:
+    print("Files in directory:")
+    for file in files:
+      print(file)
 
 #initialize the program
 if __name__ == "__main__":
